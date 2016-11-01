ad23e673174c186a3b5a3e1ece626febc7c38ec3
Samy Pesse <samypesse@gmail.com>
2016-11-02 00:41:18 +0100

    Add base normalization to move footnotes at the end

--- src/makeSchema.js
+++ src/makeSchema.js
@@ -7,8 +7,78 @@
 function makeSchema(opts) {
     return {
         rules: [
-
+            footnotesInDocument(opts),
+            footnotesAtTheEnd(opts)
         ]
+    };
+}
+
+/**
+ * Move a node to end of the document.
+ * @param  {Transform} transform
+ * @param  {Node} node
+ * @return {Transform} transform
+ */
+function moveToDocumentEnd(transform, node) {
+    const { state } = transform;
+    const { document } = state;
+    return transform.moveNodeByKey(node.key, document.key, document.nodes.size);
+}
+
+/**
+ * Rule to enforce footnotes are in the document
+ */
+function footnotesInDocument(opts) {
+    return {
+        match(node) {
+            return (node.kind === 'block');
+        },
+        validate(node) {
+            const footnotes = node.nodes.filter(child => child.type === opts.typeFootnote);
+
+            if (footnotes.isEmpty()) return;
+
+            return {
+                footnotes
+            };
+        },
+        normalize(transform, node, { footnotes }) {
+            return footnotes.reduce(moveToDocumentEnd, transform);
+        }
+    };
+}
+
+/**
+ * Rule to enforce footnotes are at the end
+ */
+function footnotesAtTheEnd(opts) {
+    const isFootnode = child => child.type === opts.typeFootnote;
+
+    return {
+        match(node) {
+            return (node.kind === 'document');
+        },
+        validate(node) {
+            const { nodes } = node;
+            const footnotesAtEnd = nodes
+                .reverse()
+                .takeWhile(isFootnode);
+
+            // Find all footnotes not at the end
+            const lastNonFootnote = nodes.size - footnotesAtEnd.size;
+            const invalids = nodes
+                .slice(0, lastNonFootnote)
+                .filter(isFootnode);
+
+            if (invalids.size === 0) return;
+
+            return {
+                invalids
+            };
+        },
+        normalize(transform, node, { invalids }) {
+            return invalids.reduce(moveToDocumentEnd, transform);
+        }
     };
 }
 
