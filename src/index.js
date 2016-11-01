c8a2cc99650bed00053298f85d315db6a2ac0a60
Samy Pesse <samypesse@gmail.com>
2016-11-01 22:59:58 +0100

    Base structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {String} opts.typeTable The type of table blocks
+ * @param {String} opts.typeRow The type of row blocks
+ * @param {String} opts.typeCell The type of cell blocks
+ */
+function EditFootnote(opts) {
+
+}
+
+module.exports = EditFootnote;
